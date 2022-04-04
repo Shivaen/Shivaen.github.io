d12e51408db9320ae560591984d2a327f02411a5
Shivaen <shivaen@gmail.com>
2022-04-03 19:27:25 -0500

    click

--- script.js
+++ script.js
@@ -40,6 +40,39 @@ function changeImagePick() {
   }
 }
 
+var counter = 6;
+function changeImagePickBack() {
+  
+  if (counter == 6) {
+    document.getElementById("Pick").src = "https://Shivaen.github.io/Pick6.jpeg";
+    counter--;
+  }
+  else if (counter == 5) {
+    document.getElementById("Pick").src = "https://Shivaen.github.io/Pick5.jpeg";
+    counter--;
+  }
+  else if (counter == 4) {
+    document.getElementById("Pick").src = "https://Shivaen.github.io/Pick4.jpeg";
+    counter--;
+  }
+  else if (counter == 3) {
+    document.getElementById("Pick").src = "https://Shivaen.github.io/Pick3.jpeg";
+    counter--;
+  }
+  else if (counter == 2) {
+    document.getElementById("Pick").src = "https://Shivaen.github.io/Pick2.jpeg";
+    counter--;
+  }
+  else if (counter == 1) {
+    document.getElementById("Pick").src = "https://Shivaen.github.io/Pick1.jpeg";
+    counter--;
+  }
+  else if (counter == 0) {
+    document.getElementById("Pick").src = "https://Shivaen.github.io/Pick7.jpeg";
+    counter = 6;
+  }
+}
+
 function openFormShelf() {
   document.getElementById("myForm-shelf").style.display = "block";
 }
@@ -58,6 +91,20 @@ function changeImageShelf() {
   else if (counter == 1) {
     document.getElementById("Shelf").src = "https://Shivaen.github.io/Shelf1.jpeg";
     counter = 0;
+  }
+  
+}
+
+var counter = 1;
+function changeImageShelfBack() {
+  
+  if (counter == 1) {
+    document.getElementById("Shelf").src = "https://Shivaen.github.io/Shelf1.jpeg";
+    counter--;
+  }
+  else if (counter == 0) {
+    document.getElementById("Shelf").src = "https://Shivaen.github.io/Shelf2.jpeg";
+    counter = 1;
   }
   
 }
