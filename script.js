e4b6c2c492d517fb187f2c17ba3ff09322e9479b
Shivaen <shivaen@gmail.com>
2022-04-05 21:31:40 -0500

    add

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 
 function openFormPick() {
   document.getElementById("myForm-pick").style.display = "block";
-  $("myForm-pick").fadeIn(1000);
 }
 
 function closeFormPick() {
@@ -107,6 +106,100 @@ function changeImageShelfBack() {
   else if (counter == 0) {
     document.getElementById("Shelf").src = "https://Shivaen.github.io/Shelf2.jpeg";
     counter = 1;
+  }
+  
+}
+
+function openFormDOTS() {
+  document.getElementById("myForm-DOTS").style.display = "block";
+}
+
+function closeFormDOTS() {
+  document.getElementById("myForm-DOTS").style.display = "none";
+}
+
+var counter = 0;
+function changeImageDOTS() {
+  
+  if (counter == 0) {
+    document.getElementById("DOTS").src = "https://Shivaen.github.io/DOTS2.jpeg";
+    counter++;
+  }
+  else if (counter == 1) {
+    document.getElementById("DOTS").src = "https://Shivaen.github.io/DOTS3.jpeg";
+    counter++;
+  }
+  else if (counter == 2) {
+    document.getElementById("Pick").src = "https://Shivaen.github.io/DOTS1.jpeg";
+    counter = 0;
+  }
+
+}
+
+var counter = 2;
+function changeImageDOTSBack() {
+  
+  if (counter == 2) {
+    document.getElementById("DOTS").src = "https://Shivaen.github.io/DOTS2.jpeg";
+    counter--;
+  }
+  else if (counter == 1) {
+    document.getElementById("DOTS").src = "https://Shivaen.github.io/DOTS1.jpeg";
+    counter--;
+  }
+  else if (counter == 0) {
+    document.getElementById("DOTS").src = "https://Shivaen.github.io/DOTS3.jpeg";
+    counter = 2;
+  }
+}
+
+function openFormED() {
+  document.getElementById("myForm-ED").style.display = "block";
+}
+
+function closeFormED() {
+  document.getElementById("myForm-ED").style.display = "none";
+}
+
+var counter = 0;
+function changeImageED() {
+  
+  if (counter == 0) {
+    document.getElementById("ED").src = "https://Shivaen.github.io/ED2.jpeg";
+    counter++;
+  }
+  else if (counter == 1) {
+    document.getElementById("ED").src = "https://Shivaen.github.io/ED3.jpeg";
+    counter++;
+  }
+  else if (counter == 2) {
+    document.getElementById("ED").src = "https://Shivaen.github.io/ED4.jpeg";
+    counter++;
+  }
+  else if (counter == 3) {
+    document.getElementById("ED").src = "https://Shivaen.github.io/ED1.jpeg";
+    counter = 0;
+  }
+}
+
+var counter = 3;
+function changeImageEDBack() {
+  
+  if (counter == 3) {
+    document.getElementById("ED").src = "https://Shivaen.github.io/ED3.jpeg";
+    counter--;
+  }
+  else if (counter == 2) {
+    document.getElementById("ED").src = "https://Shivaen.github.io/ED2.jpeg";
+    counter--;
+  }
+  else if (counter == 1) {
+    document.getElementById("ED").src = "https://Shivaen.github.io/ED1.jpeg";
+    counter--;
+  }
+  else if (counter == 0) {
+    document.getElementById("ED").src = "https://Shivaen.github.io/ED4.jpeg";
+    counter = 3;
   }
   
 }
